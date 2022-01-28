2920b83995ae547d30f572424a2344e90a3f0fb7
Cleovon <cleovon@gmail.com>
2022-01-27 22:24:13 -0300

    Novo Commit

---
+++ MyWebApiMinimal/Business/SaleBusiness.cs
@@ -0,0 +1,14 @@
+﻿using MyWebApiMinimal.Models;
+
+namespace MyWebApiMinimal.Business
+{
+    public class SaleBusiness
+    {
+
+        public Product Create(Sale sale)
+        {
+            sale.Product.Enabled = true;
+            return sale.Product;
+        }
+    }
+}
---
+++ MyWebApiMinimal/Models/Sale.cs
@@ -0,0 +1,10 @@
+﻿namespace MyWebApiMinimal.Models
+{
+    public class Sale
+    {
+        public int IdSale { get; set; }
+        public int Payment { get; set; }
+        public int Delivery { get; set; }
+        public Product Product { get; set; }
+    }
+}
